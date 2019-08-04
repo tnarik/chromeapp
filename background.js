15f9da55db153ce3c986c546cefd2ac7e8db54f1
tnarik <tnarik@gmail.com>
2019-08-04 12:20:24 +0100

    Testing integration with file system (storing retained ID, splitting selection and use of the folder).

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
 });
 
+
+// Connection from the extension
+chrome.runtime.onMessageExternal.addListener(function() {
+  chrome.app.window.create("window.html");
+});
---
+++ window.js
@@ -0,0 +1,59 @@
+let notify = function(message) {
+  // OS notification
+  chrome.notifications.create('', {
+    type: 'basic',
+    iconUrl: 'images/get_started32.png',
+    title: message,
+    message: "app "+message
+  }, function(notificationId) {});
+};
+
+let selectFolder = document.getElementById('selectFolder');
+let showFolder = document.getElementById('showFolder');
+
+let doEntry = function(entry) {
+  if (entry.isDirectory) {
+    var dirReader = entry.createReader();
+    dirReader.readEntries(function(results) {
+      console.log(results.length);
+      results.forEach(function(item) { 
+        console.log(item);
+      });
+    }, function(e){
+      console.error(e);
+    });
+  }
+};
+
+selectFolder.onclick = function(element) {
+  notify('select folder clicked');
+
+  chrome.fileSystem.chooseEntry({
+    type: 'openDirectory',
+      //suggestedName: 'todos.txt',
+      //accepts: [ { description: 'Text files (*.txt)',
+      //             extensions: ['txt']} ],
+      acceptsAllTypes: true
+    }, function(entry, fileEntries) {
+      chrome.fileSystem.getDisplayPath(entry, function(path){notify(path)});
+      var retained_id = chrome.fileSystem.retainEntry(entry);
+      chrome.storage.sync.set({ "chosenDir": retained_id });
+      console.log(retained_id);
+      console.log(fileEntries);
+      doEntry(entry);
+    }
+  );
+};
+
+showFolder.onclick = function(element) {
+  chrome.storage.sync.get('chosenDir', function(data) {
+    console.log(data.chosenDir);
+    if ( data.chosenDir ) {
+      chrome.fileSystem.isRestorable(data.chosenDir, function(isRestorable) {
+        chrome.fileSystem.restoreEntry(data.chosenDir, function(entry) {
+          doEntry(entry);
+        });
+      });
+    }
+  });
+}
